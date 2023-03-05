e6a18c5cfc644c53f24b7df4f684d01267b1bf57
letsbehuman <letsbehumandesign@gmail.com>
2023-03-05 09:46:18 +0100

    Adding navbar structure

--- src/features/navbar/navbar.tsx
+++ src/features/navbar/navbar.tsx
@@ -1,26 +1,128 @@
-import React from "react";
-import { Container } from "@mui/system";
-import { BottomNavigation, BottomNavigationAction } from "@mui/material";
-import "./navbar.scss";
-import { Grid } from "@mui/material";
+import { useState } from "react";
+import {
+  AppBar,
+  Drawer,
+  Divider,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+  Typography,
+  Button,
+} from "@mui/material";
+import Box from "@mui/material/Box";
+import MenuIcon from "@mui/icons-material/Menu";
 
-function Navbar({}) {
+const drawerWidth = 240;
+const navItems = ["Inicio", "Detalles", "Extras", "Contacto", "RSVP"];
+
+export default function Navbar() {
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const handleClick = (item: string) => {
+    console.log(item);
+  };
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        LOGO
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) =>
+          item === "RSVP" ? (
+            <Button
+              key={item}
+              sx={{ backgroundColor: "red" }}
+              onClick={() => handleClick(item)}
+            >
+              {item}
+            </Button>
+          ) : (
+            <ListItem key={item} disablePadding>
+              <ListItemButton sx={{ textAlign: "center" }}>
+                <ListItemText primary={item} />
+              </ListItemButton>
+            </ListItem>
+          )
+        )}
+      </List>
+    </Box>
+  );
+
   return (
-    <Grid columns={12} item container spacing={3}>
-      <Grid item lg={2} md={2} xs={6}>
-        <h1>logo</h1>
-      </Grid>
-      <Grid item lg={10} md={10} xs={6}>
-        <BottomNavigation showLabels>
-          <BottomNavigationAction label="Home" />
-          <BottomNavigationAction label="Details" />
-          <BottomNavigationAction label="Extra" />
-          <BottomNavigationAction label="Contact" />
-          <BottomNavigationAction label="RSVP" />
-        </BottomNavigation>
-      </Grid>
-    </Grid>
+    <Box sx={{ display: "flex" }}>
+      <AppBar
+        component="nav"
+        position="sticky"
+        sx={{ backgroundColor: "#F0A04B" }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            {navItems.map((item) =>
+              item === "RSVP" ? (
+                <Button
+                  variant="contained"
+                  key={item}
+                  sx={{ backgroundColor: "#E1EEDD", color: "black" }}
+                  onClick={() => handleClick(item)}
+                >
+                  {item}
+                </Button>
+              ) : (
+                <Button
+                  key={item}
+                  sx={{ color: "#fff" }}
+                  onClick={() => handleClick(item)}
+                >
+                  {item}
+                </Button>
+              )
+            )}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          variant="temporary"
+          anchor="top"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: "100%",
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </Box>
   );
 }
-
-export default Navbar;
