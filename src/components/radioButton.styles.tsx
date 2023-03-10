da68407dd07ce5f1f0a0316031924b55a10b7d7f
Franziska Hammerl <fran.hammerl@gmail.com>
2023-03-10 14:07:19 +0100

    styling toggleButton

--- src/components/radioButton.styles.tsx
+++
@@ -1,9 +0,0 @@
-import { Button, styled } from "@mui/material";
-
-export const option = styled(Button)({
-    color: "black",
-    "&:hover": {
-      backgroundColor: "#820000",
-    },
-  });
-  
--- src/components/radioButton.tsx
+++ src/components/radioButton.tsx
@@ -1,20 +1,45 @@
 import React, { useState } from "react";
-import { Container, Button } from "@mui/material";
+import {
+  Stack,
+  ToggleButton,
+  ToggleButtonGroup,
+} from "@mui/material";
 import { RadioButtonProps } from "../interfaces/interfaces";
+
 
 export default function RadioButton({
   data,
 }: RadioButtonProps) {
-  const [userOption, setUserOption] = useState(null);
+  const [userOption, setUserOption] = useState("");
+
+  const handleChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newOption: string
+  ) => {
+    setUserOption(newOption);
+    console.log(userOption);
+  };
+
+  const children = [
+    <ToggleButton value="si" key="si" color="primary">
+      SI
+    </ToggleButton>,
+    <ToggleButton value="no" key="no" color="secondary">
+      NO
+    </ToggleButton>,
+  ];
+
+  const control = {
+    value: userOption,
+    onChange: handleChange,
+    exclusive: true,
+  };
+
   return (
-    <Container>
-      {data.map((item) => (
-        <Button
-          onClick={() => setUserOption(item.value)}
-          variant="outlined">
-          {item.value}
-        </Button>
-      ))}
-    </Container>
+      <Stack spacing={3} alignItems="center">
+        <ToggleButtonGroup {...control}>
+          {children}
+        </ToggleButtonGroup>
+      </Stack>
   );
 }
