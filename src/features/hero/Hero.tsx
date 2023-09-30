5c33d2f14530c91e0c883c07a64ad15ecb55b7cb
letsbehuman <letsbehumandesign@gmail.com>
2023-09-30 19:19:10 +0200

    adding dinamic date

--- src/features/hero/Hero.tsx
+++ src/features/hero/Hero.tsx
@@ -1,44 +1,71 @@
 import React from "react";
 import { Container } from "@mui/system";
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Divider, Stack, Typography } from "@mui/material";
 import { CardMedia } from "@mui/material";
 import heroImage from "@assets/img/hero_img.jpg";
+import * as styles from "@hero/hero.style";
 
 function Hero({}) {
   const data = {
     nameOne: "Blanca",
     nameTwo: "Pamela",
-    date: "11 de Noviembre 2023",
+    date: "11/11/2023",
     location: "Querétaro",
+    message: "Save the date",
+    region: "es-ES",
   };
+  //* Maybe this could go in a separate file
+  const date = new Date(data.date);
+  const locale = navigator.language;
+  const options = {
+    // hour: 'numeric',
+    // minute: 'numeric',
+    // day: 'numeric',
+    // month: 'long',
+    // year: '2-digit',
+    weekday: "long" /* narrow long short*/,
+  };
+
+  const option1: DateTimeFormatOptions = {
+    weekday: "long",
+  };
+  const option2: DateTimeFormatOptions = {
+    month: "short",
+    day: "numeric",
+  };
+  const option3: DateTimeFormatOptions = {
+    year: "numeric",
+  };
+
+  const weedingDay = new Intl.DateTimeFormat(locale, option1)
+    .format(date)
+    .toUpperCase();
+  const weedingDate = new Intl.DateTimeFormat(locale, option2)
+    .format(date)
+    .toUpperCase();
+  const weedingYear = new Intl.DateTimeFormat(locale, option3).format(date);
 
   const image = heroImage;
 
   return (
-    <Box>
-      <CardMedia
-        image={image}
-        sx={{
-          height: 400,
-          color: "white",
-          display: "flex",
-          alignContent: "flex-end",
-          flexWrap: "wrap",
-          justifyContent: "center",
-        }}
-        title="The happy couple"
-      >
-        <Stack alignItems="center" spacing={2} sx={{ mb: 4 }}>
-          <Typography variant="h2" align="center" flexWrap="wrap">
-            {data.nameOne} & {data.nameTwo}
-          </Typography>
-          <Typography variant="h4">{data.location}</Typography>
-          <Typography variant="h4" align="center">
-            {data.date}
-          </Typography>
+    <CardMedia image={image} sx={styles.heroWrapper} title="The happy couple">
+      <Stack alignItems="center" spacing={{ xs: 1, sm: 2 }} sx={{ mb: 4 }}>
+        <Typography variant="h1" align="center" flexWrap="wrap">
+          {data.nameOne} & {data.nameTwo}
+        </Typography>
+        <Typography variant="h4">{data.message}</Typography>
+        {/* <Typography variant="h2">{data.location}</Typography> */}
+        <Stack
+          direction={"row"}
+          spacing={{ xs: 1, sm: 2, md: 4 }}
+          divider={<Divider orientation="vertical" flexItem />}
+        >
+          <Typography variant="h4">{weedingDay}</Typography>
+          <Typography variant="h4">{weedingDate}</Typography>
+          <Typography variant="h4">{weedingYear}</Typography>
         </Stack>
-      </CardMedia>
-    </Box>
+      </Stack>
+    </CardMedia>
   );
 }
 
---
+++ src/features/hero/hero.style.tsx
@@ -0,0 +1,8 @@
+export const heroWrapper = {
+  height: "90vh",
+  color: "white",
+  display: "flex",
+  alignContent: "flex-end",
+  flexWrap: "wrap",
+  justifyContent: "center",
+};
